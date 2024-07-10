c08fee8c7342bef04847bf154a3b5384f9e8a671
Kevin B <>
2024-07-09 18:37:25 -0600

    Committing the code for the rock paper scissors game for Odin Project practice, using JS, html and css

---
+++ rockpaperscissors/rpsjava.js
@@ -0,0 +1,57 @@
+// console.log("Hello World");
+
+
+// // function to randomly return "rock", "paper", or scissors
+
+// function getComputerChoice(){
+//     // declare a variable of selection, depending on the number generated
+
+//     let selection = Math.floor(Math.random() * 3)
+
+//     // if conditions assigned to different values
+//     if (selection === 0){
+//         return "rock"
+//     }
+//     if (selection === 1){
+//         return "paper"
+//     }
+//     if (selection === 2){
+//         return "scissors"
+//     }
+// };
+
+// console.log(getComputerChoice())
+
+// // function to have a human choice, need the prompt method
+
+// function getHumanChoice(){
+//     while(true){
+//         // prompt to use for their choice
+//         let choice = prompt("Type rock, paper or scissors").toLowerCase();
+//         // Check if the choice is valid
+//         if(choice === "rock" || choice === "paper" || choice === "scissors"){
+//             return choice;
+//         } else {
+//             alert("Invalid Choice amigo/a")
+//         }
+//     }
+// }
+
+// console.log("You chose:" + getHumanChoice())
+
+// // score tracker
+
+// let humanScore = 0;
+// let computerScore = 0;
+
+// // play round code;
+
+// function playRound(humanChoice, computerChoice) {
+//     // your code here!
+//   }
+  
+//   const humanSelection = getHumanChoice();
+//   const computerSelection = getComputerChoice();
+  
+//   playRound(humanSelection, computerSelection);
+  
