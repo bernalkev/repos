41ab1a1178346b413b82723190682a266b43bd8c
Kevin B <>
2024-07-23 20:33:34 -0600

    the rock paperscissors game from the odin project completed with all the js, css and html

--- rockpaperscissors/rpsjava.js
+++ rockpaperscissors/rpsjava.js
@@ -1,24 +1,129 @@
 // console.log("Hello World");
 
+    // // function to randomly return "rock", "paper", or scissors
+    function getRandomComputerResult() {
+        const options = ["Rock", "Paper", "Scissors"];
+        const randomIndex = Math.floor(Math.random() * options.length);
+        return options[randomIndex];
+      }
 
-// // function to randomly return "rock", "paper", or scissors
+// function to see if the player has won the round, the first to 3 wins the game
 
-// function getComputerChoice(){
-//     // declare a variable of selection, depending on the number generated
+function hasPlayerWonTheRound(player, computer) {
+    return (
+      (player === "Rock" && computer === "Scissors") ||
+      (player === "Scissors" && computer === "Paper") ||
+      (player === "Paper" && computer === "Rock")
+    );
+  }
 
-//     let selection = Math.floor(Math.random() * 3)
 
-//     // if conditions assigned to different values
-//     if (selection === 0){
-//         return "rock"
-//     }
-//     if (selection === 1){
-//         return "paper"
-//     }
-//     if (selection === 2){
-//         return "scissors"
-//     }
-// };
+let playerScore = 0;
+let computerScore = 0;
+
+
+  // getRoundResult function to update the scores
+
+  function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+  
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
+      playerScore++;
+      return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if (computerResult === userOption) {
+      return `It's a tie! Both chose ${userOption}`;
+    } else {
+      computerScore++;
+      return `Computer wins! ${computerResult} beats ${userOption}`;
+    }
+  }
+
+  // function to change the text of the elements
+
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
+  
+    if (playerScore === 3 || computerScore === 3) {
+      winnerMsgElement.innerText = `${
+        playerScore === 3 ? "Player" : "Computer"
+      } has won the game!`;
+  
+      resetGameBtn.style.display = "block";
+      optionsContainer.style.display = "none";
+    }
+  
+  };
+
+// function to reset the game
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    resetGameBtn.style.display = "hidden";
+    optionsContainer.style.display = "block";
+    roundResultsMsg.innerText = "";
+    winnerMsgElement.innerText = "";
+  };
+
+// reset game button and option buttons
+resetGameBtn.addEventListener("click", resetGame);
+
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+rockBtn.addEventListener("click", function () {
+  showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+  showResults("Scissors");
+});
+    // // function to playRound
+
+    // function playRound(playerSelection, computerSelection){
+    //     if(playerSelection === computerSelection){
+    //         return "It's a tie!";
+    //     } else if (
+    //         (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    //         (playerSelection === 'paper' && computerSelection === 'rock') ||
+    //         (playerSelection === 'scissors' && computerSelection === 'paper')
+    //     ){
+    //         return `You win ${playerSelection} beats ${computerSelection}`;
+    //     } else {
+    //         return `You lose! ${computerSelection} beats ${playerSelection}`;
+    //     }
+    // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // console.log(getComputerChoice())
 
